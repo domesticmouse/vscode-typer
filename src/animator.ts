1176f5e8239f4980f8da7e03b489f2d699e92153
Brett Morgan <brettmorgan@google.com>
2018-11-14 09:22:57 +1100

    Refactored step content out of plugin

--- src/animator.ts
+++ src/animator.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as jsdiff from 'diff';
 import * as vscode from 'vscode';
 
@@ -6,19 +7,31 @@ const heartbeatInterval = 33;
 
 export class Animator {
   private editor: vscode.TextEditor;
-  private target: string;
+  private contentPath: string;
+  private target = 'not loaded yet';
   private running = false;
 
-  constructor(editor: vscode.TextEditor, target: string) {
+  constructor(editor: vscode.TextEditor, contentPath: string) {
     this.editor = editor;
-    this.target = target;
+    this.contentPath = contentPath;
   }
 
   public start() {
     this.running = true;
-    setTimeout(() => {
-      this.heartbeat();
-    }, heartbeatInterval);
+    vscode.workspace.findFiles(this.contentPath).then((uri) => {
+      fs.readFile(uri[0].fsPath, 'UTF-8', (err, contents) => {
+        if (err) {
+          vscode.window.showErrorMessage(
+            `Failed to read ${this.contentPath}: ${err}`,
+          );
+          return;
+        }
+        this.target = contents;
+        setTimeout(() => {
+          this.heartbeat();
+        }, heartbeatInterval);
+      });
+    });
   }
   public stop() {
     this.running = false;
@@ -45,13 +58,9 @@ export class Animator {
         });
       } else if (diff.removed) {
         this.editor.edit((editBuilder) => {
-          const delta =
-            diff.value.length > charactersPerChange
-              ? charactersPerChange
-              : diff.value.length;
           const range = new vscode.Range(
             document.positionAt(cursor),
-            document.positionAt(cursor + delta),
+            document.positionAt(cursor + diff.value.length),
           );
           editBuilder.delete(range);
           changed = true;
@@ -62,6 +71,7 @@ export class Animator {
     });
     if (!changed) {
       this.running = false;
+      document.save();
     }
     setTimeout(() => {
       this.heartbeat();
